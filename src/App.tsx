8e32e39120c74629e609c8b94bcd58318e51297a
Siddharth Ganesh <ganesh.siddharth@gmail.com>
2026-06-17 15:29:25 +0530

    style(ui): add calculator container

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,43 @@
+import "./App.css";
 function App() {
  
-return (    <div>
-      <h1>NeoCalc</h1>
+return (  <div className="calculator">
+  <h1>NeoCalc</h1>
 
-      <div>0</div>
+  <div className="display">0</div>
 
-      <div>
-        <button>C</button>
-        <button>/</button>
-        <button>*</button>
-        <button>-</button>
-      </div>
+  <div className="row">
+    <button>C</button>
+    <button>/</button>
+    <button>*</button>
+    <button>-</button>
+  </div>
 
-      <div>
-        <button>7</button>
-        <button>8</button>
-        <button>9</button>
-        <button>+</button>
-      </div>
+  <div className="row">
+    <button>7</button>
+    <button>8</button>
+    <button>9</button>
+    <button>+</button>
+  </div>
 
-      <div>
-        <button>4</button>
-        <button>5</button>
-        <button>6</button>
-      </div>
+  <div className="row">
+    <button>4</button>
+    <button>5</button>
+    <button>6</button>
+  </div>
 
-      <div>
-        <button>1</button>
-        <button>2</button>
-        <button>3</button>
-        <button>=</button>
-      </div>
+  <div className="row">
+    <button>1</button>
+    <button>2</button>
+    <button>3</button>
+    <button>=</button>
+  </div>
 
-      <div>
-        <button>0</button>
-        <button>.</button>
-      </div>
-    </div>
+  <div className="row">
+    <button>0</button>
+    <button>.</button>
+  </div>
+</div>
   );
 }
 
