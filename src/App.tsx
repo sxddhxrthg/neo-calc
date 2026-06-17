161188b72018c1bd8c89d8120aa946c90c3ac82e
Siddharth Ganesh <ganesh.siddharth@gmail.com>
2026-06-17 15:15:46 +0530

    feat(ui): add calculator layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,42 @@
 function App() {
-  return (
-    <h1>NeoCalc</h1>
+ 
+return (    <div>
+      <h1>NeoCalc</h1>
+
+      <div>0</div>
+
+      <div>
+        <button>C</button>
+        <button>/</button>
+        <button>*</button>
+        <button>-</button>
+      </div>
+
+      <div>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button>+</button>
+      </div>
+
+      <div>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+      </div>
+
+      <div>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>=</button>
+      </div>
+
+      <div>
+        <button>0</button>
+        <button>.</button>
+      </div>
+    </div>
   );
 }
 
